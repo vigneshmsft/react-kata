38ac78231332157822aa1524702ee87a07bfd430
vigneshmsft <vignesh.n@outlook.com>
2019-04-03 22:30:05 +0100

    impl Hotels.all()

--- src/models/Hotels.ts
+++ src/models/Hotels.ts
@@ -1,5 +1,13 @@
 import Hotel from './Hotel';
 
 export default class Hotels {
-    
+    readonly hotels: Hotel[];
+
+    public constructor(hotels: Hotel[]) {
+      this.hotels = hotels;
+    }
+
+    public all(): Hotel[]{
+        return this.hotels;
+    }
 }
--- src/tests/Hotels.test.tsx
+++ src/tests/Hotels.test.tsx
@@ -1,19 +1,33 @@
+import Hotels from '../models/Hotels';
+
 describe('models.Hotels when given Hotel[]', () => {
+  let hotelList = [
+    {
+      name: 'hotelOne',
+      starRating: 5,
+      facilities: ['car park', 'gym', 'pool']
+    },
+    { name: 'hotelTwo', starRating: 4, facilities: ['car park', 'pool'] },
+    { name: 'hotelThree', starRating: 3, facilities: [''] }
+  ];
+  
   it('Hotels created successfully', () => {
-    expect(true).toBe(true);
+    let hotels = new Hotels(hotelList);
+    expect(hotels).not.toBe(null);
   });
 
   it('all() returns original Hotel[]', () => {
-    expect(true).toBe(true);
+    let hotels = new Hotels(hotelList);
+    expect(hotels.all().length).toBe(hotelList.length)
   });
 });
 
 describe('models.Hotels withFacilities(facilities:string[])', () => {
   it('given known facility returns only hotels with those facilities', () => {
-    expect(true).toBe(true);
+    expect(false).toBe(true);
   });
 
   it('given unknown facility returns all available hotels', () => {
-    expect(true).toBe(true);
+    expect(false).toBe(true);
   });
 });
