863505b85d5dd6c1fb30b57ec33015a6fea10388
vigneshmsft <vignesh.n@outlook.com>
2019-04-03 21:59:58 +0100

    adding test case for fetching hotels from hotelList.json

---
+++ src/tests/HotelApiClient.test.tsx
@@ -0,0 +1,10 @@
+describe('HotelApiClient when given hotelList.json getAllHotels', () => {
+  it('response is not null', () => {
+    expect(false).toBe(true);
+  });
+
+  it('response array has all hotels', () => {
+    expect(false).toBe(true);
+  });
+
+});
